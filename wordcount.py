c36bd504c13b37fd423d6f35fdbfd75eab6a49d8
Your Name <you@example.com>
2020-02-03 10:04:46 -0800

    pseudocode and created stub

---
+++ wordcount.py
@@ -0,0 +1,23 @@
+data_file = open("test.txt")
+
+
+def word_count(data_file):
+
+    """input = our text of various words
+    create empty dictionary
+    for loop to go through all of text
+    if word is not in dictionary, add it
+    if word is already in dictionary, increment occurrence by one
+    output = print out line by line word and occurrences """
+
+    word_occurrences = {}
+
+    for line in data_file:
+        line.rstrip()
+        split_paragraph = line.split(" ")
+        print(split_paragraph)
+    #     word_occurrences[word] = word_occurrences.get(word, 0) + 1
+
+    data_file.close()
+
+    return []
